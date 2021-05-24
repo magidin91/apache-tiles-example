a1c2513255f02dd4582490b9b8e04bf9e6b2db4d
Magidin <magidin@mail.ru>
2021-05-24 12:44:00 +0500

    Initial commit

---
+++ src/main/java/com/example/apachetilesexample/ApacheTilesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.apachetilesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheTilesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheTilesExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apachetilesexample/ApacheTilesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apachetilesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheTilesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
